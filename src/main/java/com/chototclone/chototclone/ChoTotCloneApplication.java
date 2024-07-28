ee36ab720aae577f2c14230d00f7d3db2aaabc7d
VoHoangKiet <vohoangkiet.dev2510@gmail.com>
2024-07-28 21:53:50 +0700

    init source

---
+++ src/main/java/com/chototclone/chototclone/ChoTotCloneApplication.java
@@ -0,0 +1,13 @@
+package com.chototclone.chototclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoTotCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoTotCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chototclone/chototclone/ChoTotCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chototclone.chototclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoTotCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
